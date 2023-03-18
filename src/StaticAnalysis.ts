2caeb2e8a2dce5bd3e101579d5028f0cd1011dec
FrederoxDev <blakefreddie96@gmail.com>
2023-03-18 19:39:55 +0000

    scope variable completions

--- src/StaticAnalysis.ts
+++ src/StaticAnalysis.ts
@@ -1,4 +1,4 @@
-import { BlockStatement, StatementCommon, VariableDeclaration } from "./Parser";
+import { BlockStatement, IfStatement, StatementCommon, VariableDeclaration, WhileStatement } from "./Parser";
 
 export type Variable = { id: string, type: string }
 
@@ -20,20 +20,24 @@ export class Scope {
  * Analyses code for type's which can be used for intellisense
  */
 export class StaticAnalysis {
-    ast: StatementCommon;
+    constructor () {}
 
-    constructor (ast: StatementCommon) {
-        this.ast = ast;
+    public getCurrentScope(index: number, scope: Scope) {
+        // Filter all children out that are already not in the scope
+        const filtered = scope.children.filter(child => index >= child.start && index <= child.end);
+
+        // Get all children scopes to remove its children outside
+        const filteredChildren = filtered.map(child => this.getCurrentScope(index, child));
+        scope.children = filteredChildren;
+
+        return scope;
     }
 
-    public getCurrentScope(index: number) {
-        // TODO: FIND THE SPECIFIC SCOPE DO NOT RETURN THE GLOBAL SCOPE!
-        return this.traverse(this.ast, new Scope(this.ast.start, this.ast.end));
-    }
-
-    private traverse(node: StatementCommon, scope: Scope): Scope {
+    public traverse(node: StatementCommon, scope: Scope): Scope {
         const types: { type: string, func: Function }[] = [
             { type: "BlockStatement", func: this.blockStatement },
+            { type: "IfStatement", func: this.ifStatement },
+            { type: "WhileStatement", func: this.whileStatement },
             { type: "VariableDeclaration", func: this.variableDeclaration }
         ]
 
@@ -43,19 +47,45 @@ export class StaticAnalysis {
             return scope;
         }
 
-        return type.func.bind(this)(node, scope);
+        scope = type.func.bind(this)(node, scope);
+        if (scope === undefined) {
+            console.log(`${node.type} did not return the scope!`)
+        }
+        return scope;
     }
 
     private blockStatement(node: BlockStatement, scope: Scope) {
         for (var i = 0; i < node.body.length; i++) {
             scope = this.traverse(node.body[i], scope);
         }
+        
+        return scope;
+    }
+
+    private ifStatement(node: IfStatement, scope: Scope) {
+        var consequent = new Scope(node.consequent.start, node.consequent.end);
+        consequent = this.traverse(node.consequent, consequent);
+        scope.children.push(consequent);
+
+        if (node.elseConsequent !== null) {
+            var elseConsequent = new Scope(node.elseConsequent.start, node.elseConsequent.end);
+            elseConsequent = this.traverse(node.elseConsequent, elseConsequent);
+            scope.children.push(elseConsequent)
+        }
+
+        return scope;
+    }
+
+    private whileStatement(node: WhileStatement, scope: Scope) {
+        var consequent = new Scope(node.consequent.start, node.consequent.end);
+        consequent = this.traverse(node.consequent, consequent);
+        scope.children.push(consequent);
+
         return scope;
     }
 
     private variableDeclaration(node: VariableDeclaration, scope: Scope) {
-        console.log(node.id, node.init.type);
-
+        // TODO: Traverse node.init
         scope.variables.push({
             id: node.id,
             type: node.init.type
@@ -63,4 +93,4 @@ export class StaticAnalysis {
 
         return scope;
     }
-}
+} 
